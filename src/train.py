b4f0e9e7a2aaf2184860300b1580c5c48749e6c4
root <root@ip-172-31-19-10.ap-south-1.compute.internal>
2026-01-29 06:58:18 +0000

    fixed Train.py

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@ import joblib
 import boto3
 import numpy as np
 import mlflow
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
